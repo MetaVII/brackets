32599bb55e6bde719284723684cdbaaed9d6804c
MetaVII <forro22@gmail.com>
2020-03-06 16:54:40 +0300

    feat: first attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const closers = new Map();
+  const stack = [];
+  bracketsConfig.forEach(element => {
+    closers.set(element[1], element[1]);
+  });
 }
